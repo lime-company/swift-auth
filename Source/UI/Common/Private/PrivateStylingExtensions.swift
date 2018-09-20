9b988489e46e096167d80553a085e63f2e5024d2
J. Kobersky <kober32@gmail.com>
2018-09-20 16:05:03 +0200

    Making UIButton styling extension public

--- Source/UI/Common/Private/PrivateStylingExtensions.swift
+++ Source/UI/Common/Private/PrivateStylingExtensions.swift
@@ -56,63 +56,6 @@ extension UIImageView {
 }
 
 
-extension UIButton {
-    
-    func applyButtonStyle(_ style: ButtonStyle?) {
-        
-        guard let style = style else {
-            return
-        }
-        if style.options.contains(.noStyle) {
-            return
-        }
-        
-        // General button
-        if let tint = style.tintColor {
-            tintColor = tint
-        }
-        if let titleColor = style.titleColor {
-            setTitleColor(titleColor.normal, for: .normal)
-            setTitleColor(titleColor.highlighted, for: .highlighted)
-            setTitleColor(titleColor.disabled, for: .disabled)
-        }
-        if let font = style.titleFont {
-            titleLabel?.font = font
-        }
-        if let bgColor = style.backgroundColor {
-            backgroundColor = bgColor.normal
-        }
-        if let image = style.image {
-            setImage(image.normal.optionalImage, for: .normal)
-            setImage(image.highlighted.optionalImage, for: .highlighted)
-            setImage(image.disabled.optionalImage, for: .disabled)
-        }
-        if let title = style.title {
-            setTitle(title, for: .normal)
-        }
-        self.adjustsImageWhenHighlighted = style.options.contains(.adjustsImageWhenHighlighted)
-        self.adjustsImageWhenDisabled = style.options.contains(.adjustsImageWhenDisabled)
-        
-        // Round corners button
-        if let rcSelf = self as? RoundCornersButton {
-            if let backgroundColor = style.backgroundColor {
-                rcSelf.highlightedBackgroundColor = backgroundColor.highlighted
-                rcSelf.disabledBackgroundColor = backgroundColor.disabled
-            }
-            if let borderColor = style.borderColor {
-                rcSelf.borderColor = borderColor.normal
-                rcSelf.highlightedBorderColor = borderColor.highlighted
-                rcSelf.disabledBorderColor = borderColor.disabled
-            }
-            rcSelf.borderCornerRadius = style.borderCornerRadius
-            rcSelf.borderWidth = style.borderWidth
-            rcSelf.isRounded = style.options.contains(.isRounded)
-        }
-    }
-    
-}
-
-
 extension UIActivityIndicatorView {
     
     func applyIndicatorStyle(_ style: ActivityIndicatorStyle?) {
