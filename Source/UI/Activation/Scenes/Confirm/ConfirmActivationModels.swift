ea0b3632eb88bbfd54fe04d845716407b0522959
J. Kobersky <kober32@gmail.com>
2019-07-10 11:16:46 +0200

    Added possibility to configure after how long is "cancel activation" presented

--- Source/UI/Activation/Scenes/Confirm/ConfirmActivationModels.swift
+++ Source/UI/Activation/Scenes/Confirm/ConfirmActivationModels.swift
@@ -21,22 +21,54 @@ public enum ConfirmActivation {
     public struct UIData {
         
         public struct Strings {
-            let sceneTitle: String
-            let sceneDescription: String
-            let waitingLabel: String
-            let removeActivationButton: String
+            public let sceneTitle: String
+            public let sceneDescription: String
+            public let waitingLabel: String
+            public let removeActivationButton: String
+            
+            public init(sceneTitle: String, sceneDescription: String, waitingLabel: String, removeActivationButton: String) {
+                self.sceneTitle = sceneTitle
+                self.sceneDescription = sceneDescription
+                self.waitingLabel = waitingLabel
+                self.removeActivationButton = removeActivationButton
+            }
         }
         
         public struct Errors {
-            let activation: String              // General error
-            let activationRemoved: String       // If activation has been removed
-            let activationBlocked: String       // If activation has been blocked
-            let passwordSetupFailure: String    // On unsuccessfull activation commit (typically fails on encryption error)
-            let recoveryFailure: String         // If it's not possible to recovery from broken activation
+            public let activation: String              // General error
+            public let activationRemoved: String       // If activation has been removed
+            public let activationBlocked: String       // If activation has been blocked
+            public let passwordSetupFailure: String    // On unsuccessfull activation commit (typically fails on encryption error)
+            public let recoveryFailure: String         // If it's not possible to recovery from broken activation
+            
+            public init(activation: String, activationRemoved: String, activationBlocked: String, passwordSetupFailure: String, recoveryFailure: String) {
+                self.activation = activation
+                self.activationRemoved = activationRemoved
+                self.activationBlocked = activationBlocked
+                self.passwordSetupFailure = passwordSetupFailure
+                self.recoveryFailure = recoveryFailure
+            }
+        }
+        
+        public struct Other {
+            /// How many tries before "Cancel Activation" button is presented.
+            /// Polling is happening every 2 seconds.
+            public let statusCheckCountBeforeCancelButton: Int
+            
+            public init(statusCheckCountBeforeCancelButton: Int) {
+                self.statusCheckCountBeforeCancelButton = statusCheckCountBeforeCancelButton
+            }
         }
         
         public let strings: Strings
         public let errors: Errors
+        public let other: Other
+        
+        public init(strings: Strings, errors: Errors, other: Other) {
+            self.strings = strings
+            self.errors = errors
+            self.other = other
+        }
     }
 }
 
