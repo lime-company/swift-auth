319ba5a913c49f54aed497d2bafe19c0c3e835ff
J. Kobersky <kober32@gmail.com>
2019-07-12 15:03:37 +0200

    Fix #89: Fixed layout issue in recovery code

--- Source/UI/Activation/Scenes/RecoveryCode/RecoveryCodeViewController.swift
+++ Source/UI/Activation/Scenes/RecoveryCode/RecoveryCodeViewController.swift
@@ -54,6 +54,14 @@ public class RecoveryCodeViewController: LimeAuthUIBaseViewController, Activatio
         addChild(vc)
         view.addSubview(vc.view)
         vc.didMove(toParent: self)
+        
+        vc.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addConstraints([
+            vc.view.topAnchor.constraint(equalTo: view.topAnchor),
+            vc.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            vc.view.leftAnchor.constraint(equalTo: view.leftAnchor),
+            vc.view.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ])
     }
     
     private func setup() {
